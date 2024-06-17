95370e8fe900b7970e6aeecfa4efa8275820de27
sandrazelen <123043915+sandrazelen@users.noreply.github.com>
2024-06-17 12:27:13 -0400

    Parses and reformats distance matrix api response

---
+++ createtable_hs_sub_distances.py
@@ -0,0 +1,67 @@
+import json
+
+def main():
+    #filename = f"C:/Users/misss/OneDrive/Desktop/csuremm/CSUREMM/output-onlinetexttools_.txt"
+    #filename =f"C:/Users/misss/Downloads/output-onlinetexttools_walking.txt"
+    #filename = f"C:/Users/misss/OneDrive/Desktop/csuremm/CSUREMM/hs_subway_walkingdist_finalversion_quotes.json"
+    filename = f"C:/Users/misss/OneDrive/Desktop/csuremm/CSUREMM/oneline_final.txt"
+    with open(filename, 'r', encoding='utf-8-sig') as f:
+        file_content = f.readlines()  
+    
+    l=[]
+    for line in file_content:
+        l.append(line)
+    #print(len(l))
+    i=0
+    for line in l:
+        #put if each line ends in , otherwise comment out
+        line =line[:-2]
+        #print(line)
+        data = json.loads(line)
+        
+        origin_addresses = data['origin_addresses']
+        destination_addresses = data['destination_addresses']
+        rows = data['rows']
+        
+        for i, row in enumerate(rows):
+            elements = row['elements']
+            for j, element in enumerate(elements):
+                distancekm = element['distance']['text']
+                distancem = element['distance']['value']
+                duration = element['duration']['text']
+                
+                # Print or process the information as needed
+                origin = origin_addresses[0]  # Since origin_addresses is a list with one item
+                destination = destination_addresses[j]
+                
+                new_dict = {
+                    'origin': origin,
+                    'destination': destination,
+                    'distancekm': distancekm,
+                    'distancem': distancem,
+                    'duration': duration
+                }
+                print(new_dict,",")
+
+if __name__ == "__main__":
+    main()
+    
+    
+"""
+    
+    for origin in d['origin_addresses']:
+        j=0
+        for destination in d['destination_addresses']:
+            measure_dict = (d['rows'][i])['elements'][j]
+            #print(measure_dict)
+            new_dict = {'origin':origin, 'destination':destination, 'distance': measure_dict['distance'], 'duration': measure_dict['duration']}
+            print(new_dict)
+            j+=1
+        
+        i+=1
+            
+if __name__ == "__main__":
+    main()
+
+"""
+
